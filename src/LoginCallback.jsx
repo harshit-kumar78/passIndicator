9e6d0691aa43418441b6ebf0d6cbf07c06f1dc15
harshit-kumar78 <harshit3551@gmail.com>
2024-08-12 22:02:02 +0530

    code refactor

--- src/LoginCallback.jsx
+++ src/LoginCallback.jsx
@@ -3,16 +3,17 @@ import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { OktaAuth } from "@okta/okta-auth-js";
 
+const oktaAuth = new OktaAuth({
+  clientId: import.meta.env.VITE_OKTA_CLIENT_ID,
+  issuer: import.meta.env.VITE_ISSUER_URI,
+  redirectUri: window.location.origin + import.meta.env.VITE_REDIRECT_URL,
+  scopes: JSON.parse(import.meta.env.VITE_SCOPE),
+  pkce: JSON.parse(import.meta.env.VITE_PKCE),
+  logLevel: import.meta.env.VITE_LOG_LEVEL,
+});
+
 const LoginCallback = () => {
   const navigate = useNavigate();
-  const oktaAuth = new OktaAuth({
-    clientId: import.meta.env.VITE_OKTA_CLIENT_ID,
-    issuer: import.meta.env.VITE_ISSUER_URI,
-    redirectUri: window.location.origin + import.meta.env.VITE_REDIRECT_URL,
-    scopes: JSON.parse(import.meta.env.VITE_SCOPE),
-    pkce: JSON.parse(import.meta.env.VITE_PKCE),
-    logLevel: import.meta.env.VITE_LOG_LEVEL,
-  });
 
   useEffect(() => {
     oktaAuth
